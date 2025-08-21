1fc0823a426fd288778fce714768dc2a629334b4
Thompson Knuth <thompsonmknuth@gmail.com>
2025-08-21 09:43:10 +0200

    Update NFL Player Offense Ratings (Adjusted to 2000s standard).py
    Adjusted time sleep to avoid getting kicked.

--- NFL Player Offense Ratings (Adjusted to 2000s standard).py
+++ NFL Player Offense Ratings (Adjusted to 2000s standard).py
@@ -48,7 +48,7 @@ for i in range(1932,2025):
     data['Year']=i
     total_data.append(data)
     print(f"{i} passing stats added.")
-    time.sleep(5)
+    time.sleep(10)
 
 #%%
 # formatting qb data
@@ -272,7 +272,7 @@ for i in range(1932,2025):
     a_df['Year']=i
     total_rb.append(a_df)
     print(f"{i} rushing stats added.")
-    time.sleep(5)
+    time.sleep(10)
 
 rb_df = pd.concat(total_rb,ignore_index=True)
 a_rb_df = rb_df.iloc[1:].copy()
@@ -402,7 +402,7 @@ for i in range(1932,2025):
     df_wr['Year']=i
     total_wr.append(df_wr)
     print(f"{i} receiving stats added.")
-    time.sleep(5)
+    time.sleep(10)
 
 wr_df = pd.concat(total_wr,ignore_index=True)
 
@@ -572,5 +572,6 @@ offense_final = pd.concat(offense_df_list,ignore_index=True)
 perfect_offense = offense_final.loc[offense_final['rating_adj_1']==158.3]
 
 # offense_final.to_csv(r'C:\Users\thompson.knuth\Desktop\Webscraping Practice\NFL\NFL Offensive Ratings Data Build (2000s) (TK).csv')
+
 
 
